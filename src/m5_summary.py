19be5ce951a84bb0466cf21278ebf0f321cff3aa
robertml2 <robertm2@rose-hulman.edu>
2018-03-05 09:35:38 -0500

    Commit

--- src/m5_summary.py
+++ src/m5_summary.py
@@ -2,10 +2,10 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Madison Robertson.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -27,3 +27,25 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ###############################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+window.delay(30)
+
+liz = rg.SimpleTurtle()
+liz.speed = 3
+liz.forward(20)
+liz.right(30)
+liz.forward(50)
+liz.right(50)
+liz.forward(100)
+liz.right(130)
+liz.forward(100)
+liz.right(50)
+liz.forward(50)
+liz.right(30)
+liz.forward(20)
+liz.right(40)
+liz.forward(10)
+window.close_on_mouse_click()
